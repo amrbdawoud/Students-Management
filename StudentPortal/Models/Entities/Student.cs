335634dfab94d2e38f356ada8b7c6ef447e2c533
Amr Dawoud <amrdawoud23@gmail.com>
2024-03-09 18:33:14 +0200

    First Commit, all CRUD functionality

---
+++ StudentPortal/Models/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentPortal.Models.Entities
+{
+    public class Student
+    {
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Boolean Subscribed { get; set; }
+    }
+}
